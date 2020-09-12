27c18d43270e888723b2304966b2677292f0b07f
OmgCopito95 <july1995bolt@gmail.com>
2020-09-11 22:35:38 -0300

    recorridos totales en un arbol

---
+++ IMPERATIVO/ARBOLES/recorridos_totales_arbol.pas
@@ -0,0 +1,34 @@
+// RECORRIDOS TOTALES DE UN ARBOL
+
+// EN ORDEN (Imprime de menor a mayor - Para imprimir de mayor a menor, invierto la recursion)
+
+procedure enOrden (a:arbol);
+begin
+	if (a <> nil) then begin
+		enOrden(a^.HI);
+		write('', a^.dato);
+		enOrden(a^.HD);	
+	end;	
+end;
+
+// POST ORDEN 
+
+procedure postOrden (a:arbol);
+begin
+	if (a <> nil) then begin
+		postOrden(a^.HI);
+		postOrden(a^.HD);
+		write('', a^.dato);
+	end;	
+end;
+
+// PRE ORDEN (jerarquico)
+
+procedure preOrden (a:arbol);
+begin
+	if (a <> nil) then begin
+		write('', a^.dato);
+		preOrden(a^.HI);
+		preOrden(a^.HD);		
+	end;	
+end;
