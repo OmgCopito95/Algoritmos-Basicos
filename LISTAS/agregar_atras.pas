90588c25f4c24b493c24f1abb53034433c811301
OmgCopito95 <july1995bolt@gmail.com>
2020-09-11 22:50:52 -0300

    dos maneras de agregar atras en una lista

---
+++ LISTAS/agregar_atras.pas
@@ -0,0 +1,37 @@
+// AGREGAR AL FINAL EN UNA LISTA
+
+// OPCION 1 - recorre la lista para encontrar el ultimo elemento
+
+procedure agregarAtras(var l:lista; dato: integer);
+var
+	nue, act, ant: lista;
+begin
+	new(nue);
+	nue^.dato := dato;
+	ant := l;
+	act := l;
+	while (act <> nil) do begin
+		ant := act;
+		act := act^.sig;
+	end;
+	if (ant = act) then
+		l := nue
+	else
+		ant^.sig := nue;
+	nue^.sig := act;
+end;
+
+// OPCION 2 - con puntero al ultimo elemento
+
+procedure agregarAtras(var l: lista; var ult: lista; dato: integer);
+ver
+begin
+	new(nue);
+	nue^.dato := dato;
+	nue^.sig := nil;
+	if (l = nil) then
+		l := nue
+	else
+		ult^.sig := nue;
+	ult := nue;	
+end;
