7e975b8928855319ee1fdfe01b849a2172ba7335
OmgCopito95 <july1995bolt@gmail.com>
2020-09-11 22:34:52 -0300

    calcular elemento maximo de un arbol

---
+++ IMPERATIVO/ARBOLES/calcular_maximo.pas
@@ -0,0 +1,12 @@
+// CALCULAR MAXIMO DE UN ARBOL
+
+function Maximo (a:arbol): arbol;
+begin
+	if (a = nil) then
+		Maximo := nil
+	else
+		if (a^.HD = nil) then
+			Maximo := a
+		else
+			Maximo := Maximo(a^.HD); 
+end;
