a1de1f505f2e3fc14ca3e5726eb33cde79942663
OmgCopito95 <july1995bolt@gmail.com>
2021-03-08 09:20:48 -0300

    agrega recorrido completo en arbol

--- IMPERATIVO/ARBOLES/buscar_en_arbol.pas
+++ IMPERATIVO/ARBOLES/buscar_en_arbol.pas
@@ -1,4 +1,4 @@
-// BUSCAR EN UN ARBOL. Devuelve un puntero de tipo arbol. Si no lo encuentra devuelve nil
+// BUSCAR EN UN ARBOL RESPETANDO EL ORDEN. Devuelve un puntero de tipo arbol. Si no lo encuentra devuelve nil
 
 function Buscar (a:arbol; x:integer): arbol;
 begin
@@ -13,3 +13,52 @@ begin
 			else
 				Buscar := Buscar(a^.HD, x)
 end;
+
+// BUSCAR EN UN ARBOL SIN RESPETAR EL ORDEN. Devuelve un puntero de tipo arbol. Si no lo encuentra devuelve nil
+
+function Buscar (a:arbol; x:integer): arbol;
+begin
+	if (a = nil) then
+		Buscar := nil
+	else
+		if (x = a^.dato) then
+			Buscar := a
+		else
+                    Buscar := Buscar(a^.HI, x) or Buscar(a^.HD, x);
+
+end;
+
+
+procedure buscar (a:arbol; x: integer; var ok:boolean);
+begin
+        if (a=nil)then
+                ok:=false
+        else
+            if (a^.dato=x)then
+                    ok:=true
+            else begin
+              buscar(a^.HI,x,ok);
+              if (not ok) then
+                      buscar(a^.HD,x,ok)
+            end;
+
+end;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
