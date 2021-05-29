f05052fe08d3e473de5259f42419af895bcfaa80
OmgCopito95 <july1995bolt@gmail.com>
2021-05-29 16:13:54 -0300

    Eliminar elemento de una lista

---
+++ IMPERATIVO/LISTAS/eliminar_elemento.pas
@@ -0,0 +1,45 @@
+//EL ELEMENTO PUEDE NO EXISTIR EN LA LISTA
+
+Procedure BorrarElemento (var pri:lista; nom:cadena50; var exito: boolean);
+var ant, act: lista;
+begin 
+  exito := false;
+  act := pri;
+  {Recorro mientras no se termine la lista y no encuentre el elemento}
+  while  (act <> NIL)  and (act^.datos.nom <> nom) do begin
+      ant := act;
+      act := act^.sig
+  end;   
+  if (act <> NIL) then begin
+       exito := true; 
+	  if (act = pri) then  
+      pri := act^.sig;
+    else  
+      ant^.sig:= act^.sig;
+    dispose (act);
+  end;
+end;
+
+
+
+
+//EL ELEMENTO SEGURO EXISTE EN LA LISTA
+
+Procedure BorrarElemento (var pri:lista; nom:cadena50; var exito: boolean);
+var ant, act: lista;
+begin 
+  exito := false;
+  act := pri;
+  {Recorro mientras no se termine la lista y no encuentre el elemento}
+  while (act^.datos.nom <> nom) do begin
+      ant := act;
+      act := act^.sig
+  end;   
+  
+  exito := true; 
+  if (act = pri) then  
+      pri := act^.sig;
+  else  
+      ant^.sig:= act^.sig;
+  dispose (act);
+end;
