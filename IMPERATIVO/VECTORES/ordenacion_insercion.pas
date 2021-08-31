f688f1a461a877b7391502cffce2d99722dd6166
Copito <july1995bolt@gmail.com>
2021-08-30 22:53:09 -0300

    cambio dimF por dimL

--- IMPERATIVO/VECTORES/ordenacion_insercion.pas
+++ IMPERATIVO/VECTORES/ordenacion_insercion.pas
@@ -1,17 +1,17 @@
 // ORDENACION DE VECTORES POR INSERCION
 
-procedure OrdenacionPorInsercion(var v: vector);
+procedure OrdenacionPorInsercion(var v: vector; dimL: integer);
 var 
 	i, j, dato: integer;
 begin
 
-	for i:= 2 to dimF do begin
+	for i:= 2 to dimL do begin
    		actual:=v[i];
    		j:= i-1;
    		while (j > 0) and (v[j] > actual) do begin
-     		v[j+1]:=v[j];
-     		j := j-1;
-     	end;
+     			v[j+1]:=v[j];
+     			j := j-1;
+     		end;
 		v[j+1] := actual;
 	end;
 end;
