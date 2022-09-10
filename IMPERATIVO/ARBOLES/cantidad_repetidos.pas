b852a4436203f7a36f7f39fc7972702db547b273
OmgCopito95 <july1995bolt@gmail.com>
2022-09-10 19:21:04 -0300

    cantidad repetidos en arbol por diferente criterio

--- IMPERATIVO/ARBOLES/cantidad_repetidos.pas
+++ IMPERATIVO/ARBOLES/cantidad_repetidos.pas
@@ -1,5 +1,6 @@
-// CONTAR CANTIDAD DE VECES QUE APARECE UN ELEMENTO EN EL ARBOL
+// CONTAR CANTIDAD DE VECES QUE APARECE UN ELEMENTO EN EL ARBOL 
 
+// POR MISMO CRITERIO DE CREACION
 function Cantidad_Repetidos (a:arbol; x:integer): integer;
 begin
 	if (a = nil) then 
@@ -12,4 +13,19 @@ begin
 				Buscar := Buscar(a^.HI, x)
 			else
 				Buscar := Buscar(a^.HD, x)
+end;
+
+
+// POR DIFERENTE CRITERIO
+function cant_repe (a:arbol; x:integer): integer;
+begin
+	if (a = nil) then
+		cant_repe := 0;
+	else
+	begin
+		if (a^.dato = x) then
+			cant_repe := (cant_repe(a^.HI, x) + cant_repe(a^.HD, x) + 1) // SI EL DATO ES IGUAL A X, SE SUMA 1
+		else
+			cant_repe := (cant_repe(a^.HI, x) + cant_repe(a^.HD, x)) // SI EL DATO ES DISTINTO A X, NO SE SUMA NADA y SE CONTINUA RECORRIENDO EL ARBOL
+	end;
 end;
